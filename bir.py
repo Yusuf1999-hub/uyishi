9313a21b9312cb7453805e2ffb4de10d9eae998b
Yusuf <yusufkhusanovic@gmail.com>
2026-05-21 16:02:39 +0500

    nimadur bo'ldi

--- bir.py
+++ bir.py
@@ -3,3 +3,9 @@ a = 90
 b = 10
 c = a + b
 print(c)
+
+
+allajsdlfjalsdj lambdaasdlfjaskldjlfkjaskldjflkasjdf
+alsdjflajs;ldfj;laskdj;lfkjas;ldkjf;lkasjd;lfj
+asldfjaskldj;flksjadl;fkjas;kldjfklasjklfjaslkflasjljf
+aslkdfjlaskdjf;lkasjd;lfkjasd;klfj
